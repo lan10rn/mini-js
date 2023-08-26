d0c79f247496a082094362e78959e6e499174cf1
Abhinav Chaudhary <91717130+lan10rn@users.noreply.github.com>
2023-08-26 16:04:15 +0530

    Add files via upload

---
+++ gradient_maker/gradient.js
@@ -0,0 +1,32 @@
+let colorOne = document.getElementById("color-a");
+let colorTwo = document.getElementById("color-b");
+let currentDirection = "to bottom";
+let outputCode = document.getElementById("code");
+
+function setDirection(value, _this) {
+  let direcrtions = document.querySelectorAll(".buttons button");
+  for (let i of direcrtions) {
+    i.classList.remove("active");
+  }
+  _this.classList.add("active");
+  currentDirection = value;
+}
+
+function generateCode() {
+  outputCode.value = `background-image: linear-gradient(${currentDirection}, ${colorOne.value}, ${colorTwo.value})`;
+  document.getElementsByTagName(
+    "BODY"
+  )[0].style.backgroundImage = `linear-gradient(${currentDirection}, ${colorOne.value}, ${colorTwo.value})`;
+}
+
+function copyText() {
+    outputCode.select();
+    document.execCommand("copy");
+  var x = document.getElementById("cpyBar");
+  x.className = "show";
+  setTimeout(function () {
+    x.className = x.className.replace("show", "");
+  }, 3000);
+}
+
+generateCode();
